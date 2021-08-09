05e18e2c49eb2a0f22cb422c1fa2a1e22c6b8c6d
Shubham <shubhamkumarwinner@gmail.com>
2021-08-09 16:37:04 +0530

    LinearProgressIndicator

--- app/src/main/java/com/shubhamkumarwinner/composecomponents/MainActivity.kt
+++ app/src/main/java/com/shubhamkumarwinner/composecomponents/MainActivity.kt
@@ -29,9 +29,45 @@ class MainActivity : ComponentActivity() {
             ComposeComponentsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ExtendedFloatingActionButtonDemo()
+                    LinearProgressIndicatorSample()
                 }
             }
+        }
+    }
+}
+
+//linearProgressIndicator
+@Composable
+fun LinearProgressIndicatorSample() {
+    var progress by remember { mutableStateOf(0.1f) }
+    val animatedProgress = animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    ).value
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Spacer(Modifier.height(30.dp))
+        Text("LinearProgressIndicator with undefined progress")
+        LinearProgressIndicator()
+        Spacer(Modifier.height(30.dp))
+        Text("LinearProgressIndicator with progress set by buttons")
+        LinearProgressIndicator(progress = animatedProgress)
+        Spacer(Modifier.height(30.dp))
+        OutlinedButton(
+            onClick = {
+                if (progress < 1f) progress += 0.1f
+            }
+        ) {
+            Text("Increase")
+        }
+
+        OutlinedButton(
+            onClick = {
+                if (progress > 0f) progress -= 0.1f
+            }
+        ) {
+            Text("Decrease")
         }
     }
 }
@@ -242,6 +278,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeComponentsTheme {
-        ExtendedFloatingActionButtonDemo()
+        LinearProgressIndicatorSample()
     }
 }
